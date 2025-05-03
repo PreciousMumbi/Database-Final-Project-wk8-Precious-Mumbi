8bde2346fadc3b2344f3d22ab939b1bf9ef9ef54
PreciousMumbi <mumbiprecious026@gmail.com>
2025-05-04 01:27:57 +0300

    Update database.py

--- task_manager/database.py
+++ task_manager/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/task_manager"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Chris2012@localhost/task_manager"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
